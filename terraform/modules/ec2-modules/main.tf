a4298667aa1808e10d3623f4f35359604cb2b1e6
Antwi-tech <nanaafuaantwiwaa624@gmail.com>
2025-12-29 14:24:22 +0000

    terraform files

---
+++ terraform/modules/ec2-modules/main.tf
@@ -0,0 +1,73 @@
+# resource "aws_key_pair" "my_wtf_key" {
+#     public_key = file("")
+# }
+
+# resource "aws_security_group" "my_wtf_sg" {
+   
+# }
+
+# resource "aws_instace" "my_wtf_instance" {
+  
+# }
+
+locals {
+  # normalized workspace name
+  environment = terraform.workspace == "default" ? "dev" : terraform.workspace
+ 
+  # base app/project name
+  project     = "wtf"
+ 
+  # standard prefix pattern for all resources
+  prefix      = "${local.project}-${local.environment}"
+}
+
+resource "aws_instance" "my_wtf_server" {
+    associate_public_ip_address = true
+    key_name = aws_key_pair.my_wtf_key.key_name
+    instance_type = var.instance_type
+    ami = data.aws_ami.my_wtf_ami.id
+    subnet_id = var.subnet_id
+    region = var.region
+    availability_zone = var.az
+    vpc_security_group_ids = [aws_security_group.wtf_sg.id]
+    user_data =  file("ec2-modules/script.sh")
+
+    tags ={
+          Name= "${local.prefix}-server"
+    }
+}
+
+resource "aws_key_pair" "my_wtf_key" {
+   key_name = "my_wtf_key"
+   public_key = file("ec2-modules/my_key.pub")
+  
+}
+
+resource "aws_security_group" "wtf_sg" {
+  name =" ${local.prefix}-sg"
+  description = "anything"
+  vpc_id = var.vpc_id
+
+  ingress {
+    from_port = 22
+    to_port =  22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
---
+++ terraform/modules/ec2-modules/variables.tf
@@ -0,0 +1,19 @@
+variable "instance_type" {
+  
+}
+
+variable "vpc_id" {
+  
+}
+
+variable "region" {
+  
+}
+
+variable "az" {
+  
+}
+
+variable "subnet_id" {
+  
+}
